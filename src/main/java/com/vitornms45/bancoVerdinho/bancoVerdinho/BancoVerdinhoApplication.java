5b07b378aa7948849521c0f8ff2dad4cd5ed06ce
Victor Nunes Moraes Silva <vitornones25@gmail.com>
2025-09-22 12:12:13 -0300

    first commit

---
+++ src/main/java/com/vitornms45/bancoVerdinho/bancoVerdinho/BancoVerdinhoApplication.java
@@ -0,0 +1,13 @@
+package com.vitornms45.bancoVerdinho.bancoVerdinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoVerdinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoVerdinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitornms45/bancoVerdinho/bancoVerdinho/BancoVerdinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitornms45.bancoVerdinho.bancoVerdinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoVerdinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
